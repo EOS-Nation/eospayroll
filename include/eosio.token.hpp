91958390815c98a315cea78627b20d95ec055792
Jimmy Mack <52579871+jmacktacular@users.noreply.github.com>
2019-08-12 20:04:59 -0400

    Update eosio.token.hpp
    Cleaned and added comments for recent changes

--- include/eosio.token.hpp
+++ include/eosio.token.hpp
@@ -1,178 +1,261 @@
-#pragma once
-
 #include <eosio/asset.hpp>
+#include <eosio/symbol.hpp>
 #include <eosio/eosio.hpp>
+#include <eosio/print.hpp>
+#include <eosio/system.hpp>
+#include <eosio.token.hpp>
 
-#include <string>
+using namespace eosio;
+using namespace std;
 
-namespace eosiosystem {
-    class system_contract;
-}
-
-namespace eosio {
-
-    using std::string;
+class [[eosio::contract("eospayroll")]] eospayroll : public eosio::contract {
+public:
+    using contract::contract;
 
     /**
-     * @defgroup eosiotoken eosio.token
-     * @ingroup eosiocontracts
+     * Construct a new contract given the contract name
      *
-     * eosio.token contract
-     *
-     * @details eosio.token contract defines the structures and actions that allow users to create, issue, and manage
-     * tokens on eosio based blockchains.
-     * @{
+     * @param {name} receiver - The name of this contract
+     * @param {name} code - The code name of the action this contract is processing
+     * @param {datastream} ds - The datastream used
      */
-    class [[eosio::contract("eosio.token")]] token : public contract {
-        public:
-            using contract::contract;
+    eospayroll( name receiver, name code, eosio::datastream<const char*> ds )
+        : contract( receiver, code, ds ),
+            _currency( receiver, receiver.value ),
+            _payroll( receiver, receiver.value ),
+            _payout( receiver, receiver.value )
+    {}
 
-            /**
-             * Create action.
-             *
-             * @details Allows `issuer` account to create a token in supply of `maximum_supply`.
-             * @param issuer - the account that creates the token,
-             * @param maximum_supply - the maximum supply set for the token created.
-             *
-             * @pre Token symbol has to be valid,
-             * @pre Token symbol must not be already created,
-             * @pre maximum_supply has to be smaller than the maximum supply allowed by the system: 1^62 - 1.
-             * @pre Maximum supply must be positive;
-             *
-             * If validation is successful a new entry in statstable for token symbol scope gets created.
-             */
-            [[eosio::action]]
-            void create( const name&   issuer,
-                             const asset&  maximum_supply);
-            /**
-             * Issue action.
-             *
-             * @details This action issues to `to` account a `quantity` of tokens.
-             *
-             * @param to - the account to issue tokens to, it must be the same as the issuer,
-             * @param quntity - the amount of tokens to be issued,
-             * @memo - the memo string that accompanies the token issue transaction.
-             */
-            [[eosio::action]]
-            void issue( const name& to, const asset& quantity, const string& memo );
+    /**
+     * ACTION `setcurrency`
+     *
+     * Set currency rates
+     *
+     * @param {asset} rate - currency rate based on EOS price
+     * @example
+     *
+     * setcurrency( "4.56 CAD" );
+     */
+    [[eosio::action]]
+    void setcurrency( asset rate );
 
-            /**
-             * Retire action.
-             *
-             * @details The opposite for create action, if all validations succeed,
-             * it debits the statstable.supply amount.
-             *
-             * @param quantity - the quantity of tokens to retire,
-             * @param memo - the memo string to accompany the transaction.
-             */
-            [[eosio::action]]
-            void retire( const asset& quantity, const string& memo );
+    /**
+     * ACTION `rmvcurrency`
+     *
+     * Removes currency rate from EOS payroll
+     *
+     * @param {symbol_code} currency - EOSIO symbol code currency
+     * @example
+     *
+     * rmvcurrency( "CAD" );
+     */
+    [[eosio::action]]
+    void rmvcurrency( symbol_code currency );
 
-            /**
-             * Transfer action.
-             *
-             * @details Allows `from` account to transfer to `to` account the `quantity` tokens.
-             * One account is debited and the other is credited with quantity tokens.
-             *
-             * @param from - the account to transfer from,
-             * @param to - the account to be transferred to,
-             * @param quantity - the quantity of tokens to be transferred,
-             * @param memo - the memo string to accompany the transaction.
-             */
-            [[eosio::action]]
-            void transfer( const name&    from,
-                                const name&    to,
-                                const asset&   quantity,
-                                const string&  memo );
-            /**
-             * Open action.
-             *
-             * @details Allows `ram_payer` to create an account `owner` with zero balance for
-             * token `symbol` at the expense of `ram_payer`.
-             *
-             * @param owner - the account to be created,
-             * @param symbol - the token to be payed with by `ram_payer`,
-             * @param ram_payer - the account that supports the cost of this action.
-             *
-             * More information can be read [here](https://github.com/EOSIO/eosio.contracts/issues/62)
-             * and [here](https://github.com/EOSIO/eosio.contracts/issues/61).
-             */
-            [[eosio::action]]
-            void open( const name& owner, const symbol& symbol, const name& ram_payer );
+    /**
+     * ACTION `addpayroll`
+     *
+     * Add payroll details for payee's payouts per interval period
+     *
+     * @param {name} from - account name of sender
+     * @param {name} to - account name of payee
+     * @param {asset} quantity - quantity amount to be paid per payout period
+     * @param {string} memo - memo used when sending transfer
+     * @param {uint32_t} interval - minimum payout interval in seconds (ex: 60 * 60 * 24 * 7 = 604800 = 1 week)
+     * @example
+     *
+     * addpayroll( "sender.accnt", "payee.accnt", "100.00 CAD", "weekly salary", 604800 );
+     */
+    [[eosio::action]]
+    void addpayroll( name from,
+                        name to,
+                        asset quantity,
+                        string memo,
+                        uint32_t interval );
 
-            /**
-             * Close action.
-             *
-             * @details This action is the opposite for open, it closes the account `owner`
-             * for token `symbol`.
-             *
-             * @param owner - the owner account to execute the close action for,
-             * @param symbol - the symbol of the token to execute the close action for.
-             *
-             * @pre The pair of owner plus symbol has to exist otherwise no action is executed,
-             * @pre If the pair of owner plus symbol exists, the balance has to be zero.
-             */
-            [[eosio::action]]
-            void close( const name& owner, const symbol& symbol );
+    /**
+     * ACTION `rmvpayroll`
+     *
+     * Removes payroll from EOS payroll table
+     *
+     * @param {uint64_t} id - payroll unique identifier
+     * @example
+     *
+     * rmvpayroll( 0 );
+     */
+    [[eosio::action]]
+    void rmvpayroll( uint64_t id );
 
-            /**
-             * Get supply method.
-             *
-             * @details Gets the supply for token `sym_code`, created by `token_contract_account` account.
-             *
-             * @param token_contract_account - the account to get the supply for,
-             * @param sym_code - the symbol to get the supply for.
-             */
-            static asset get_supply( const name& token_contract_account, const symbol_code& sym_code )
-            {
-                stats statstable( token_contract_account, sym_code.raw() );
-                const auto& st = statstable.get( sym_code.raw() );
-                return st.supply;
-            }
+    /**
+     * ACTION `payout`
+     *
+     * Performs payouts per sender's payroll
+     *
+     * `eosio.code@active` must be defined in sender's permission
+     *
+     * @param {name} from - account name of sender which authorizes payroll
+     * @example
+     *
+     * cleos push action <code> payout ["sender.acct"]
+     */
+    [[eosio::action]]
+    void payout( name from );
 
-            /**
-             * Get balance method.
-             *
-             * @details Get the balance for a token `sym_code` created by `token_contract_account` account,
-             * for account `owner`.
-             *
-             * @param token_contract_account - the token creator account,
-             * @param owner - the account for which the token balance is returned,
-             * @param sym_code - the token for which the balance is returned.
-             */
-            static asset get_balance( const name& token_contract_account, const name& owner, const symbol_code& sym_code )
-            {
-                accounts accountstable( token_contract_account, owner.value );
-                const auto& ac = accountstable.get( sym_code.raw() );
-                return ac.balance;
-            }
+    /**
+     * Notify contract when eosio.token deposits core symbol
+     *
+     * Used for token swap
+     */
+    [[eosio::on_notify("eosio.token::transfer")]]
+    void transfer( const name&    from,
+                   const name&    to,
+                   const asset&   quantity,
+                   const string&  memo );
 
-            using create_action = eosio::action_wrapper<"create"_n, &token::create>;
-            using issue_action = eosio::action_wrapper<"issue"_n, &token::issue>;
-            using retire_action = eosio::action_wrapper<"retire"_n, &token::retire>;
-            using transfer_action = eosio::action_wrapper<"transfer"_n, &token::transfer>;
-            using open_action = eosio::action_wrapper<"open"_n, &token::open>;
-            using close_action = eosio::action_wrapper<"close"_n, &token::close>;
-        private:
-            struct [[eosio::table]] account {
-                asset    balance;
+    using payout_action = eosio::action_wrapper<"payout"_n, &eospayroll::payout>;
 
-                uint64_t primary_key()const { return balance.symbol.code().raw(); }
-            };
+private:
+    /**
+     * TABLE `currency`
+     *
+     * Contains all info related to the currency rates
+     *
+     * @param {symbol_code} currency - currency code
+     * @param {asset} rate - rate of currency valued in EOS
+     * @param {time_point_sec} timestamp - last time currency rate was updated
+     *
+     * @example
+     * {
+     *   "currency": "CAD",
+     *   "rate": "4.56 CAD",
+     *   "timestamp": "2019-07-30T22:21:51"
+     * }
+     */
+    struct [[eosio::table("currency")]] currency_row {
+        symbol_code    	currency;
+        asset     		rate;
+        time_point_sec 	timestamp;
 
-            struct [[eosio::table]] currency_stats {
-                asset    supply;
-                asset    max_supply;
-                name     issuer;
-
-                uint64_t primary_key()const { return supply.symbol.code().raw(); }
-            };
-
-            typedef eosio::multi_index< "accounts"_n, account > accounts;
-            typedef eosio::multi_index< "stat"_n, currency_stats > stats;
-
-            void sub_balance( const name& owner, const asset& value );
-            void add_balance( const name& owner, const asset& value, const name& ram_payer );
+        auto primary_key() const { return currency.raw(); }
     };
-    /** @}*/ // end of @defgroup eosiotoken eosio.token
-} /// namespace eosio
+
+    /**
+     * TABLE `payroll`
+     *
+     * Contains all info related to the payroll
+     *
+     * @param {uint64_t} id - unique identifier of payroll
+     * @param {name} from - account name of sender
+     * @param {name} to - account name of payee
+     * @param {asset} quantity - quantity amount to be paid per payout period
+     * @param {string} memo - memo used when sending transfer
+     * @param {uint32_t} interval - minimum payout interval in seconds (ex: 60 * 60 * 24 * 7 = 604800 = 1 week)
+     * @param {time_point_sec} timestamp - last time payroll was paid
+     *
+     * @example
+     * {
+     *   "id": 0,
+     *   "from": "sender.accnt",
+     *   "to": "payee.accnt",
+     *   "quantity": "100.00 CAD",
+     *   "memo": "weekly salary",
+     *   "interval": 604800,
+     *   "timestamp": "2019-07-30T22:21:51"
+     * }
+     */
+    struct [[eosio::table("payroll")]] payroll_row {
+        uint64_t        id;
+        name			from;
+        name			to;
+        asset			quantity;
+        string			memo;
+        uint32_t		interval;
+        time_point_sec	timestamp;
+
+        auto 		primary_key() const { return id; }
+        uint64_t  	by_from() const { return from.value; }
+        uint64_t  	by_to() const { return to.value; }
+    };
+
+    /**
+     * TABLE `payout`
+     *
+     * Contains all info related to the payout
+     *
+     * @param {uint64_t} id - unique identifier of payout
+     * @param {checksum256} transaction - identifier for transaction
+     * @param {name} from - account name of sender
+     * @param {name} to - account name of payee
+     * @param {asset} quantity - quantity amount to be paid per payout period
+     * @param {asset} rate - rate of currency valued in EOS
+     * @param {asset} eos_quantity - quantity of EOS to be paid
+     * @param {string} memo - memo used when sending transfer
+     * @param {time_point_sec} timestamp - last time payout was paid
+     *
+     * @example
+     * {
+     *   "id": 0,
+     *   "transaction": "8dba3ce623551b766efe77db7b3b9151627a3bb6bfa8b5fc1609ad8a152c83bd",
+     *   "from": "sender.accnt",
+     *   "to": "payee.accnt",
+     *   "quantity": "100.00 CAD",
+     *   "rate": "5.65 CAD",
+     *   "eos_quantity": "10.0000 EOS",
+     *   "memo": "weekly salary",
+     *   "timestamp": "2019-07-30T22:21:51"
+     *   
+     * }
+     */
+    struct [[eosio::table("payout")]] payout_row {
+        uint64_t        id;
+        checksum256		transaction;
+        name			from;
+        name			to;
+        asset			quantity;
+        asset 			rate;
+        asset			eos_quantity;
+        string			memo;
+        time_point_sec	timestamp;
+
+        auto 		primary_key() const { return id; }
+    };
+
+    // Multi-indexes
+    using currency_table = multi_index <"currency"_n, currency_row>;
+    using payout_table = multi_index <"payout"_n, payout_row>;
+
+    typedef multi_index<"payroll"_n, payroll_row,
+        indexed_by<"byfrom"_n, const_mem_fun<payroll_row, uint64_t, &payroll_row::by_from> >,
+        indexed_by<"byto"_n, const_mem_fun<payroll_row, uint64_t, &payroll_row::by_to> >
+    > payroll_table;
+
+    currency_table _currency;
+    payroll_table _payroll;
+    payout_table _payout;
+
+    // private methods
+
+    // currency
+    bool currency_exists( symbol_code currency );
+    void check_currency_exists( symbol_code currency );
+    void emplace_currency( asset rate );
+    void modify_currency( asset rate );
+    void erase_currency( symbol_code currency );
+    asset zero_currency( symbol_code currency );
+    asset get_currency_rate( symbol_code currency );
+
+    // payroll
+    uint64_t emplace_payroll( name from, name to, asset quantity, string memo, uint32_t interval );
+    void erase_payroll( uint64_t id );
+    bool payroll_exists( uint64_t id );
+    void check_payroll_exists( uint64_t id );
+    void update_payroll_timestamp( uint64_t id );
+
+    // payout
+    uint64_t emplace_payout( name from, name to, asset quantity, asset eos_quantity, string memo, asset rate );
+    time_point_sec calculate_payout_timestamp( uint32_t interval );
+    void transfer_eosio_token( name from, name to, asset quantity, string memo );
+
+    // utils
+    checksum256 get_trx_id();
+};
